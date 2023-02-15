cba24faa204fdf035fed6be285780505c884a358
Sara Morison <rob.morison32@gmail.com>
2023-02-15 15:40:02 -0500

    Initialized PostList component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React, { useState } from 'react';
 import Post from './Components/Post/post';
+import PostList from './Components/PostList/PostList';
 
 function App() {
 
   const [postList, setPostList] = useState([])
 
+  function addNewEntry(entry){
+    let tempEntries =[entry];
+    setPostList(tempEntries);
+  }
+
+
+
   return (
     <div>
       <Post />
+      <PostList />
     </div>
   );
 }
---
+++ src/Components/PostList/PostList.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+const PostList = (props) => {
+    return ( 
+        <table className='table'>
+            
+        </table>
+     );
+}
+ 
+export default PostList;
